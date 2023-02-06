e405cff5a2b39ec8dd22cd784ee55e1894c4e1ea
Michalis <57771939+MichelStath@users.noreply.github.com>
2023-02-06 07:42:44 +0200

    Translate Stats

--- app/src/main/java/com/example/myapplication/StatsActivity.java
+++ app/src/main/java/com/example/myapplication/StatsActivity.java
@@ -3,12 +3,52 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StatsActivity extends AppCompatActivity {
+    TextView totalFireAlertsTV,totalRainAlertsTV,totalHotAlertsTV,totalAlertsTV,
+                totalFireAlertsTodayTV,totalRainAlertsTodayTV,totalHotAlertsTodayTV,totalAlertsTodayTV,
+                totalFireAlertsTodayCityTV,totalRainAlertsTodayCityTV,totalHotAlertsTodayCityTV,totalAlertsTodayCityTV;
+    int totalFireAlertsCounter, totalRainAlertsCounter, totalHotAlertsCounter,totalAlertsCounter,
+            totalFireAlertsTodayCounter,totalRainAlertsTodayCounter,totalHotAlertsTodayCounter,totalAlertsTodayCounter,
+            totalFireAlertsTodayCityCounter,totalRainAlertsTodayCityCounter,totalHotAlertsTodayCityCounter,totalAlertsTodayCityCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+
+        //region LAYOUT ITEMS
+        totalFireAlertsTV = findViewById(R.id.totalFireAlerts);
+        totalRainAlertsTV = findViewById(R.id.totalRainAlerts);
+        totalHotAlertsTV = findViewById(R.id.totalHotAlerts);
+        totalAlertsTV = findViewById(R.id.totalAlerts);
+        totalFireAlertsTodayTV = findViewById(R.id.totalFireAlertsToday);
+        totalRainAlertsTodayTV = findViewById(R.id.totalRainAlertsToday);
+        totalHotAlertsTodayTV = findViewById(R.id.totalHotAlertsToday);
+        totalAlertsTodayTV = findViewById(R.id.totalAlertsToday);
+        totalFireAlertsTodayCityTV = findViewById(R.id.totalFireAlertsTodayCity);
+        totalRainAlertsTodayCityTV = findViewById(R.id.totalRainAlertsTodayCity);
+        totalHotAlertsTodayCityTV = findViewById(R.id.totalHotAlertsTodayCity);
+        totalAlertsTodayCityTV = findViewById(R.id.totalAlertsTodayCity);
+        //endregion
+
+        //getStats();
     }
+
+    public void getStats(){
+        totalFireAlertsTV.setText(String.valueOf(totalFireAlertsCounter));
+        totalRainAlertsTV.setText(String.valueOf(totalRainAlertsCounter));
+        totalHotAlertsTV.setText(String.valueOf(totalHotAlertsCounter));
+        totalAlertsTV.setText(String.valueOf(totalAlertsCounter));
+        totalFireAlertsTodayTV.setText(String.valueOf(totalFireAlertsTodayCounter));
+        totalRainAlertsTodayTV.setText(String.valueOf(totalRainAlertsTodayCounter));
+        totalHotAlertsTodayTV.setText(String.valueOf(totalHotAlertsTodayCounter));
+        totalAlertsTodayTV.setText(String.valueOf(totalAlertsTodayCounter));
+        totalFireAlertsTodayCityTV.setText(String.valueOf(totalFireAlertsTodayCityCounter));
+        totalRainAlertsTodayCityTV.setText(String.valueOf(totalRainAlertsTodayCityCounter));
+        totalHotAlertsTodayCityTV.setText(String.valueOf(totalHotAlertsTodayCityCounter));
+        totalAlertsTodayCityTV.setText(String.valueOf(totalAlertsTodayCityCounter));
+    }
+
 }
